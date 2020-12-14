48cd408a76dda57a73bc5151076de78dd798ac5a
Nicholas Maher <nicholas.s.maher@gmail.com>
2020-12-14 08:52:57 -0500

    initial commit & programmatic view setup

---
+++ VenueFinder/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//  VenueFinder
+//
+//  Created by Nicholas Maher on 12/14/20.
+//  Copyright © 2020 Nicholas Maher. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            self.window?.makeKeyAndVisible()
+        }
+
+        return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            self.window = window
+            
+            let viewController = VenueSearchVC()
+            window.rootViewController = UINavigationController(rootViewController: viewController)
+        }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ VenueFinder/VenueSearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VenueFinder
+//
+//  Created by Nicholas Maher on 12/14/20.
+//  Copyright © 2020 Nicholas Maher. All rights reserved.
+//
+
+import UIKit
+
+class VenueSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
