e47796bf9b0de9bd1c00b9dcdb34e4510da0a0e5
Nicholas Maher <nicholas.s.maher@gmail.com>
2020-12-14 23:43:05 -0500

    add globals, start work on getVenues functionality

--- VenueFinder/Services/FoursquareService.swift
+++ VenueFinder/Services/FoursquareService.swift
@@ -8,16 +8,45 @@
 
 import Foundation
 
-//Singleton
+// Singleton
 class FoursquareService {
     static let shared = FoursquareService()
     
     private init() { }
     
+    fileprivate var versionDate: String = {
+        let dateFormatter = DateFormatter()
+        let date = Date(timeIntervalSince1970: 1606796389) // 12/1/2020
+        dateFormatter.dateFormat = "yyyyMMdd"
+        return dateFormatter.string(from: date)
+    }()
+    
     public func getVenues(atLatitude lat: Double,
                           atLongitude long: Double,
                           forQuery query: String,
-                          completion: @escaping([Venue], Error) -> Void) {
+                          completion: @escaping([Venue]?, Error) -> Void) {
         
+        let baseUrl = URL(string: "https://api.foursquare.com/v2/venues/search")!
+        let queryParams: [String: String] = [
+            "ll": "\(lat),\(long)",
+            "query": query,
+            "limit": "20",
+            "cliend_id": FOURSQUARE_CLIENT_ID,
+            "client_secret": FOURSQUARE_CLIENT_SECRET,
+            "v": versionDate
+        ]
+        
+        let url = baseUrl.withQueries(queryParams)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data, let venues = try? jsonDecoder.decode(Response.self, from: data), let error = error {
+                completion(venues.response.venues, error)
+            } else if let error = error {
+                completion(nil, error)
+            }
+            
+        }
+        task.resume()
     }
 }
